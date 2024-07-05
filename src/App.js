4c1e78eb6443b261c66eb7b5abd8c5aa7c716f20
Aarthi Edara <aarthiedara@Aarthis-Air.lan>
2024-07-04 22:14:14 -0400

    4th

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ function App() {
       
       <Routes>
         {/* <Route exact path="/main" element={<Main />} /> */}
-        {/* <Route path="/" element={<Main />} />  */}
+        <Route path="/" element={<Main />} /> 
 
-        <Route path="/ULTRA_APP/" element={<Main />} />
+        <Route path="/ULTRA_APP" element={<Main />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
