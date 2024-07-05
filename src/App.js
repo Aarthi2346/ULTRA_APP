a16d7b7e692acc34510dede58de15c785e207bf4
Aarthi2346 <61651021+Aarthi2346@users.noreply.github.com>
2024-07-04 22:21:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       {/* <div className="top-left-container">
         <img src={ucfImage} className="App-logo" alt="UCF" />
         </div>        */}
-        <Link to="/ULTRA_APP" className="App-link"><h2> ← Return to Main page</h2></Link>
+        <Link to="/ULTRA_APP/" className="App-link"><h2> ← Return to Main page</h2></Link>
         </header>
       
       <NavigationBar />
@@ -30,7 +30,7 @@ function App() {
         {/* <Route exact path="/main" element={<Main />} /> */}
         <Route path="/" element={<Main />} /> 
 
-        <Route path="/ULTRA_APP" element={<Main />} />
+        <Route path="/ULTRA_APP/" element={<Main />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
